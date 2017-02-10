b054455a83dcf4aff27e3eaa3f8582fe789afa2d
Cheton Wu <cheton@gmail.com>
2017-02-10 12:58:25 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ if [ "$TRAVIS" = "true" ]; then
     git config --global user.email "cncjs@cncjs.org"
     git config --global user.name "cncjs"
     ./node_modules/.bin/gh-pages \
+        --silent \
         --repo https://$GITHUB_TOKEN@github.com/cncjs/cncjs.org.git \
         --dist public
-        #--silent \
 else
     ./node_modules/.bin/gh-pages --dist public
 fi
